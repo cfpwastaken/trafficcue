a3919ead45de1e9ddb22b7a658bb4874392d5bb8
Cfp <chaosflo44plus@gmail.com>
2025-06-22 15:23:55 +0200

    chore: update lnv host

--- src/lib/services/hosts.ts
+++ src/lib/services/hosts.ts
@@ -4,4 +4,4 @@ export const ROUTING_SERVER = "https://valhalla1.openstreetmap.de/";
 // export const ROUTING_SERVER = "https://routing.map.picoscratch.de";
 export const SEARCH_SERVER = "https://photon.komoot.io/";
 export const OVERPASS_SERVER = "https://overpass-api.de/api/interpreter";
-export const LNV_SERVER = "http://localhost:3000/api";
+export const LNV_SERVER = location.hostname == "localhost" ? "http://localhost:3000/api" : "https://trafficcue-api.picoscratch.de/api";
