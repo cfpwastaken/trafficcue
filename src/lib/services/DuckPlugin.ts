5e23d0fa61bd58b16e94c0155bca379324ac1807
Cfp <chaosflo44plus@gmail.com>
2025-06-25 14:01:21 +0200

    fix: add dummy web DuckPlugin

--- src/lib/services/DuckPlugin.ts
+++ src/lib/services/DuckPlugin.ts
@@ -1,10 +1,19 @@
-import { registerPlugin } from "@capacitor/core";
+import { registerPlugin, WebPlugin } from "@capacitor/core";
 
 export interface DuckPlugin {
 	duck: () => void;
 	unduck: () => void;
 }
 
-const Duck = registerPlugin<DuckPlugin>("Duck");
+class DuckWeb extends WebPlugin implements DuckPlugin {
+	// eslint-disable-next-line @typescript-eslint/no-empty-function
+	duck(): void {}
+	// eslint-disable-next-line @typescript-eslint/no-empty-function
+	unduck(): void {}
+}
+
+const Duck = registerPlugin<DuckPlugin>("Duck", {
+	web: new DuckWeb()
+});
 
 export default Duck;
