cc1d5ba32e8b0b4dbab5bb0d05fb80bf4e1fd873
Cfp <jannik@picoscratch.de>
2025-08-06 11:24:33 +0200

    feat: add tauri

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,32 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import path from "path";
 
+const host = process.env.TAURI_DEV_HOST;
+
 export default defineConfig({
 	plugins: [tailwindcss(), svelte()],
+	clearScreen: false,
+	server: {
+		port: 5173,
+		strictPort: true,
+		host: host || false,
+		hmr: host ? {
+			protocol: "ws",
+			host,
+			port: 1421
+		} : undefined,
+		watch: {
+			ignored: ["**/src-tauri/**"]
+		}
+	},
+	envPrefix: ["VITE_", "TAURI_ENV_*"],
+	build: {
+		target: process.env.TAURI_ENV_PLATFORM == "windows"
+			? "chrome105"
+			: "safari13",
+		minify: !process.env.TAURI_ENV_DEBUG ? "esbuild" : false,
+		sourcemap: !!process.env.TAURI_ENV_DEBUG
+	},
 	resolve: {
 		alias: {
 			$lib: path.resolve("./src/lib"),
