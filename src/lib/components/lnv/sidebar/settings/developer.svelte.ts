f9d34d942e15116fe58a2d05b8771d7f8675de9b
Cfp <jannik@picoscratch.de>
2025-08-11 09:52:36 +0200

    fix: hide dev options toggle

--- src/lib/components/lnv/sidebar/settings/developer.svelte.ts
+++ src/lib/components/lnv/sidebar/settings/developer.svelte.ts
@@ -1,11 +1,11 @@
 export function getDeveloperToggle() {
 	const value = localStorage.getItem("developer")
 	const state = $state({
-		current: value == null ? false : value
+		current: value == null ? "false" : value
 	});
 
 	$effect(() => {
-		localStorage.setItem("developer", JSON.stringify(state.current));
+		localStorage.setItem("developer", state.current);
 	});
 
 	return state;
