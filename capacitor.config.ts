8c33ff7c887c410bb5259c0442a5c08c66684a59
Jannik <jannik@picoscratch.de>
2025-09-13 17:46:24 +0200

    feat: remove unused capacitor dependencies

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from "@capacitor/cli";
-
-const config: CapacitorConfig = {
-	appId: "de.trafficcue.trafficcue",
-	appName: "TrafficCue",
-	webDir: "dist",
-};
-
-export default config;
